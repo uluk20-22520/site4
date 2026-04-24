77032e1b57596d65737d31af3d1c7e2ca1482f29
uluk20-22520 <118132058+uluk20-22520@users.noreply.github.com>
2026-04-24 11:55:11 +0600

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -1,5 +1,5 @@
 window.ULUK_CLOUD_CONFIG = {
-  useCloud: false,
+  useCloud: true,
   supabaseUrl: 'https://fugdkjmifvvnjldquksz.supabase.co',
   supabaseAnonKey: 'sb_publishable_SrgLwKb4YWNdbCX8bA1sAw_P3sKZv3n',
   tableName: 'leads',
